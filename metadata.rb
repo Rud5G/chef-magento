7e99fbf53c06506ed1a2c97ceb3adac8f7747302
Rudger Gravestein <rudgergravestein@gmail.com>
2014-06-14 01:04:17 +0200

    Add Changelog

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license          'Apache 2.0'
 description      'Installs/Configures magento'
 long_description 'Installs/Configures magento'
-version          '0.1.0'
+version          '0.1.1'
 
-depends 'zf2', '~> 0.3.2'
+depends 'zf2', '~> 0.4.2'
 
